0fd01a9b30924877a5f3d2055c32f2f6821a7b46
Andy-Knight <39732302+Andy-Knight@users.noreply.github.com>
2020-08-04 16:36:08 +0100

    Delete test2.py

--- test2.py
+++
@@ -1,9 +0,0 @@
-def handler(context, inputs):
-    greeting = "Hello, {0}!".format(inputs["target"])
-    print(greeting)
-
-    outputs = {
-      "greetings123": greeting
-    }
-
-    return outputs
