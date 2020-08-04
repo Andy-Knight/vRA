67268209d6d5123f36f36929d90a8887faecb03d
Andy-Knight <39732302+Andy-Knight@users.noreply.github.com>
2020-08-04 17:17:10 +0100

    Update testaction.py

--- Actions/testaction.py
+++ Actions/testaction.py
@@ -9,7 +9,7 @@ def handler(context, inputs):
     :return The desired machine name.
     """
     old_name = inputs["resourceNames"][0]
-    new_name = inputs["customProperties"]["newName"]
+    new_name = inputs["customProperties"]["newName123"]
 
     outputs = {}
     outputs["resourceNames"] = inputs["resourceNames"]
