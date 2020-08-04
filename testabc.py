e47b1edeee57907f663d071f6383a28d8cd2407b
Andy-Knight <39732302+Andy-Knight@users.noreply.github.com>
2020-08-04 16:41:30 +0100

    Delete testabc.py

--- testabc.py
+++
@@ -1,9 +0,0 @@
-def handler(context, inputs):
-    greeting = "Hello, {0}!".format(inputs["target"])
-    print(greeting)
-
-    outputs = {
-      "greetingss": greeting
-    }
-
-    return outputs
